5a8811d338f83b2084a55a6496bd34c80ad3e9e7
McTimon <tnyaffram@gmail.com>
2022-10-21 23:20:25 +0000

    add name gen script

---
+++ src/Class.js
@@ -0,0 +1,14 @@
+export default class Class {
+  constructor() {
+    //to do
+    // this.area = area;
+  }
+
+  static area(side) {
+    return side * side;
+  }
+}
+
+// let area = (side) => side * side;
+
+// export { area };
---
+++ src/nameScript.js
@@ -0,0 +1,35 @@
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let nick = "";
+for (let i = 0; i < letters.length; i++) {
+  i += 2;
+  nick += letters[i];
+  i += 1;
+}
+console.log(nick);
